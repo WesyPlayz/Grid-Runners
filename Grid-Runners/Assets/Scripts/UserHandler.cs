c0906297ec4af396a8e2cc85940c7640dd1d95e4
stunbot11 <zachary@borda.us>
2024-02-27 20:24:01 -0800

    added melee along with some scripts

--- Grid-Runners/Assets/Scripts/UserHandler.cs
+++ Grid-Runners/Assets/Scripts/UserHandler.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class UserHandler : MonoBehaviour
 {
+    //objects
     public GameObject secondary_Obj;
-
     private Rigidbody obj_Physics;
     private Transform obj_Transform;
+
+    //script data
+    private Obj_State obj_Data;
+    private Obj_State secondary_Data;
+    private DumbiesHandler dumby;
 
     private bool is_Jumping;
 
@@ -20,6 +25,9 @@ public class UserHandler : MonoBehaviour
 
     private void Start()
     {
+        obj_Data = GetComponent<Obj_State>();
+        secondary_Data = secondary_Obj.GetComponent<Obj_State>();
+
         obj_Physics = GetComponent<Rigidbody>();
         obj_Transform = GetComponent<Transform>();
     }
@@ -29,32 +37,33 @@ public class UserHandler : MonoBehaviour
         if (on_Floor)
         { 
             obj_Physics.AddRelativeForce(Vector3.down * 5);
-            obj_Physics.velocity = new Vector3(Input.GetAxis("Horizontal") * 5, obj_Physics.velocity.y, Input.GetAxis("Vertical") * 5);
+            obj_Physics.velocity = new Vector3(Input.GetAxis("Horizontal") * obj_Data.Speed, obj_Physics.velocity.y, Input.GetAxis("Vertical") * obj_Data.Speed);
         }
 
         if (can_Attack) 
         {
-            /*
-            if (secondary_Data.collided_Entity != null && Input.GetAxis("RT") != 0)
+            
+            if (secondary_Data.collided_Entity != null && Input.GetAxis("Knife") != 0)
             {
-                Debug.Log("trigger pulled");
                 can_Attack = false;
                 MeleeAttack();
-                StartCoroutine(AttackCooldown(obj_Data.melee_Cooldown));
+                StartCoroutine(AttackCooldown(obj_Data.Attack_Cooldown));
             }
-            */
+            
         }
     }
 
 
-    /*
+    
     void MeleeAttack()
     {
+        /*
         if (secondary_Data.collided_Entity.name == "Enemy")
         {
             secondary_Data.collided_Entity.GetComponent<EnemyHandler>().damaged_Particle.GetComponent<ParticleSystem>().Play();
             //secondary_Data.collided_Entity.GetComponent<EnemyHandler>().damaged_Sound.Play(); //needs sxf
         }
+        */
 
         if (secondary_Data.collided_Entity.name == "Dumbie")
         {
@@ -62,14 +71,9 @@ public class UserHandler : MonoBehaviour
             dumby.Begin();
         }
 
-        Obj_State entity_Data = secondary_Data.collided_Entity.GetComponent<Obj_State>();
-        entity_Data.Health = Mathf.Max(entity_Data.Health - obj_Data.Damage, 0); // subtracts the damage amount to health, and doesn't allow health to go below 0 if it does.
 
-        obj_Data.Energy = Mathf.Min(obj_Data.Energy +
-            (entity_Data.Health <= 0 ? Random.Range(obj_Data.mix_Energy_Increase * 5, obj_Data.max_Energy_Increase * 2) :
-            Random.Range(obj_Data.mix_Energy_Increase, obj_Data.max_Energy_Increase)), obj_Data.max_Energy);
     }
-    */
+    
 
     /*
     IEnumerator JumpCooldown()
