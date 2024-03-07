ab6053f2140ae1dcb2509e20d5bc578785547d43
WesyPlayz <149786874+WesyPlayz@users.noreply.github.com>
2024-03-06 23:44:14 -0800

    Medium Patch
    Tutorial_Map Update + Movement Bug Fix + Performance Enhancement

---
+++ Grid-Runners/Assets/Scripts/Utility_Scripts/Item_Data.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Data : MonoBehaviour
+{
+    [System.Serializable]
+    public class Weapon
+    {
+        public GameObject weapon_Prefab;
+
+        public int upgrade_Lvl;
+
+        [Header("Melee")]
+        public bool is_Melee;
+
+        [Header("Ranged")]
+        public bool is_Ranged;
+
+        public GameObject projectile;
+        public GameObject fire_Point;
+
+        public ParticleSystem muzzle_Flash;
+
+        public float fire_Rate;
+
+        public int max_Ammo;
+    }
+    public List<Weapon> Weapons;
+
+    [System.Serializable]
+    public class Ordinance
+    {
+        public GameObject ordinance_Prefab;
+
+        public ParticleSystem explosion;
+    }
+    public List<Ordinance> Ordinances;
+}
