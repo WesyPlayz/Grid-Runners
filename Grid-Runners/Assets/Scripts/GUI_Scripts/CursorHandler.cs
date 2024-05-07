6e6a856feae79daa3a232f3eb5662db9b98f8a74
thefunniestlol <352278@students.bethelsd.org>
2024-05-07 13:43:35 -0700

    goobadoobado

--- Grid-Runners/Assets/Scripts/GUI_Scripts/CursorHandler.cs
+++ Grid-Runners/Assets/Scripts/GUI_Scripts/CursorHandler.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
 using TMPro;
-
 using System.Security.Cryptography.X509Certificates;
-
 public class CursorHandler : MonoBehaviour
 {
     [SerializeField] private List<Button> Buttons = new List<Button>();
